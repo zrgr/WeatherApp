aa04e7e8a20ffd195bf395fe242e37a2c4cdd0f6
Zak <zrodger7@gmail.com>
2022-06-30 21:35:23 +0100

    Added dependencies for secrets gradle plugin, modified current weather api call

--- app/src/main/java/com/example/weather/network/WeatherApi.kt
+++ app/src/main/java/com/example/weather/network/WeatherApi.kt
@@ -1,13 +1,14 @@
 package com.example.weather.network
 
+import com.example.weather.BuildConfig
 import com.example.weather.models.Weather
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
-    @GET("apod")
-    suspend fun getApod(
-        @Query("api_key")
-        apiKey: String = API_KEY
+    @GET("current.json")
+    suspend fun getWeather(
+        @Query("key")
+        key: String = BuildConfig.API_KEY
     ): Weather
 }
--- app/src/main/java/com/example/weather/utils/Constants.kt
+++ app/src/main/java/com/example/weather/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.example.weather.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://api.weatherapi.com/v1/current.json"
+        const val BASE_URL = "http://api.weatherapi.com/v1/"
     }
 }
