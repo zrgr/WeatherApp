c3a0c0fd8d312a885c9d46ac6508e25859e17fb3
Zak <zrodger7@gmail.com>
2022-07-01 23:16:31 +0100

    Work on jacket fragment, enabled data binding

--- app/src/main/java/com/example/weather/fragments/JacketFragment.kt
+++ app/src/main/java/com/example/weather/fragments/JacketFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.weather.R
+import com.example.weather.WeatherViewModel
+import com.example.weather.databinding.FragmentJacketBinding
 
 class JacketFragment : Fragment() {
+
+    private val viewModel: WeatherViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +22,15 @@ class JacketFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jacket, container, false)
+        val binding = FragmentJacketBinding.inflate(inflater)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 
 }
