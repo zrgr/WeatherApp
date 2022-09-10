8e547f0134e381a199f1de0023d251beffa46ce8
Zak <zrodger7@gmail.com>
2022-09-11 00:59:17 +0100

    Location can now be clicked

--- app/src/main/java/com/example/weather/fragments/JacketFragment.kt
+++ app/src/main/java/com/example/weather/fragments/JacketFragment.kt
@@ -1,19 +1,31 @@
 package com.example.weather.fragments
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.os.Handler
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.method.LinkMovementMethod
+import android.text.style.*
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.text.toSpannable
 import androidx.fragment.app.activityViewModels
 import com.example.weather.FutureWeatherGridAdapter
 import com.example.weather.FutureWeatherListener
 import com.example.weather.R
 import com.example.weather.WeatherViewModel
 import com.example.weather.databinding.FragmentJacketBinding
+import com.example.weather.models.app.Weather
+import com.example.weather.models.app.WeatherForecast
 import com.github.matteobattilana.weather.PrecipType
 
 
@@ -32,17 +44,19 @@ class JacketFragment : Fragment() {
         binding.viewModel = viewModel
 
         viewModel.weather.observe(viewLifecycleOwner) { data ->
-            val chanceOfRain = data.weatherToDisplay.chanceOfRain
+
             val currentWindSpeed = data.weatherToDisplay.windSpeed.toInt()
-            val location = data.locationName
+
             binding.jacket.text = data.weatherToDisplay.jacketNeeded
-            binding.currentChanceRain.text = getString(R.string.current_chance_rain, chanceOfRain, location)
+
             binding.weatherType.setImageResource(data.weatherToDisplay.weatherTypeImage)
             binding.temperature.text = getString(R.string.temperature, data.weatherToDisplay.temperature)
             binding.temperatureFeelsLike.text = getString(R.string.temperature_feels_like, data.weatherToDisplay.temperatureFeelsLike)
             binding.windSpeed.text = getString(R.string.wind_speed, data.weatherToDisplay.windSpeed)
-            binding.weatherTypeDescription.text = "Heavy Rain showers"//getString(data.weatherToDisplay.weatherTypeDescription)
-            setWeather(chanceOfRain.toFloat(), currentWindSpeed)
+            binding.weatherTypeDescription.text = getString(data.weatherToDisplay.weatherTypeDescription)
+
+            createChanceOfRainText(data)
+            setWeather(data.weatherToDisplay.chanceOfRain.toFloat(), currentWindSpeed)
         }
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
@@ -64,5 +78,42 @@ class JacketFragment : Fragment() {
             colour = Color.parseColor("#0214fa")
             scaleFactor = 3f
         }
+    }
+
+    private fun createChanceOfRainText(weather: WeatherForecast) {
+        val chanceOfRain = weather.weatherToDisplay.chanceOfRain
+        val location = weather.locationName
+        var chanceOfRainText = getString(R.string.current_chance_rain, chanceOfRain, location)
+        val spannable = SpannableStringBuilder(chanceOfRainText)
+        val highlightStartPos = chanceOfRainText.length - location.length
+        val highlightEndPos = chanceOfRainText.length
+
+        spannable.setSpan(
+            ForegroundColorSpan(Color.GREEN),
+            highlightStartPos, highlightEndPos,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+
+        spannable.setSpan(
+            UnderlineSpan(),
+            highlightStartPos, highlightEndPos,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+
+        val changeLocation = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                Toast.makeText(view.context, "Location Clicked!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        spannable.setSpan(
+            changeLocation,
+            highlightStartPos, highlightEndPos,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+
+        binding.currentChanceRain.text = spannable
+        binding.currentChanceRain.movementMethod = LinkMovementMethod.getInstance()
+        binding.currentChanceRain.highlightColor = Color.TRANSPARENT
     }
 }
